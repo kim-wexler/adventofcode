57b59ac80a89caac7440cdbd2aeea9e49b317e5b
Kim Wexler <sft365@gmail.com>
2022-12-03 11:51:05 +0000

    day 3 complete

---
+++ 2022/day3/day3.py
@@ -0,0 +1,13 @@
+import sys
+
+data = sys.stdin.read().strip().split("\n")
+
+F = lambda x: ord(x) - 96 if x >= "a" else ord(x) - 38
+
+pt1 = [set(l[:len(l)//2]) & set(l[len(l)//2:]) for l in data]
+pt1ans = [next(map(F, r)) for r in pt1]
+print(sum(pt1ans))
+
+pt2 = [set.intersection(*[set(g) for g in data[i:i+3]]) for i in range(0, len(data), 3)]
+pt2ans = [next(map(F, r)) for r in pt2]
+print(sum(pt2ans))
