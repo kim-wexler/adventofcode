ce74549f88eb9b78da660a43942b44b8a1e7508c
Kim Wexler <sft365@gmail.com>
2022-12-06 11:53:47 +0000

    day6 refactored

--- 2022/day6/day6.py
+++ 2022/day6/day6.py
@@ -2,10 +2,7 @@ import sys
 
 data = sys.stdin.read().strip()
 
-def solve(data, marker_len):
-    for i in range(len(data)-marker_len):
-        if len(set(data[i:i+marker_len])) == marker_len:
-            return i + marker_len
-    return 0
+def solve(data, n):
+    return next(i+n for i in range(len(data)) if len(set(data[i:i+n])) == n)
 
 print(solve(data, 4), solve(data, 14))
