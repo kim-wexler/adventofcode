6a2f353bd4dcea27b18e00a101951d87b7143945
Kim Wexler <sft365@gmail.com>
2022-12-09 12:19:22 +0000

    refactored part9

--- 2022/day9/day9.py
+++ 2022/day9/day9.py
@@ -1,12 +1,12 @@
 import sys
 
 data = sys.stdin.read().splitlines()
-data = [line.split(" ") for line in data]
+data = [(a, int(b)) for a, b in [line.split(" ") for line in data]]
 
-def set_pos(D):
-    for i in range(len(D)-2, -1, -1):
-        x, y = D[i+1]
-        dx, dy = D[i]
+def set_pos(pos):
+    for i in range(len(pos)-2, -1, -1):
+        x, y = pos[i+1]
+        dx, dy = pos[i]
         if abs(x-dx) > 1 or abs(y-dy) > 1:
             if x > dx:
                 dx += 1
@@ -16,44 +16,20 @@ def set_pos(D):
                 dy += 1
             elif y < dy:
                 dy -= 1
-            D[i] = (dx, dy)
+            pos[i] = (dx, dy)
 
-tail = set()
-tail2 = set()
+dirs = {"R": (1, 0), "L": (-1, 0), "U": (0, 1), "D": (0, -1)}
+tailpt1, tailpt2 = set(), set()
 x = y = 0
-dx = dy = 0
-pos = [(0,0)] * 9
-for d in data:
-    (a, b) = d[0], int(d[1])
+pos = [(0,0)] * 10
+for (a, b) in data:
+    c = dirs[a]
     for i in range(b):
-        if a == "R":
-            x += 1
-        if a == "L":
-            x -= 1
-        if a == "U":
-            y += 1
-        if a == "D":
-            y -= 1
-        if abs(x-dx) > 1 or abs(y-dy) > 1:
-            if x > dx:
-                dx += 1
-            elif x < dx:
-                dx -= 1
-            if y > dy:
-                dy += 1
-            elif y < dy:
-                dy -= 1
-            pos[-1] = (dx, dy)
-            set_pos(pos)
-        tail.add(pos[0])
-        tail2.add(pos[-1])
-print(len(tail), len(tail2))
+        x += c[0]
+        y += c[1]
+        pos[-1] = (x, y)
+        set_pos(pos)
+        tailpt1.add(pos[-2])
+        tailpt2.add(pos[0])
 
-# X = 4, 2
-# T = 3, 0
-
-# X = 2, 2
-# T = 4, 1
-
-X = 4, 1
-T = 3, 0
+print(len(tailpt1), len(tailpt2))
